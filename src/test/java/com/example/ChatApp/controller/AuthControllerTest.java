16d4849d44408ebf6de76ff3c43866eb25ee593e
Vidya68 <vidya.22110068@viit.ac.in>
2025-08-14 14:02:36 +0530

    user friendly- login and registration message

--- src/test/java/com/example/ChatApp/controller/AuthControllerTest.java
+++ src/test/java/com/example/ChatApp/controller/AuthControllerTest.java
@@ -4,6 +4,7 @@ import com.example.ChatApp.dto.user.UpdateProfileRequest;
 import com.example.ChatApp.dto.user.UserResponse;
 import com.example.ChatApp.entity.User;
 import com.example.ChatApp.repositories.UserRepository;
+import com.example.ChatApp.security.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -15,6 +16,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,37 +40,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
         @Autowired
         private ObjectMapper objectMapper;
-
+        @Autowired
+        private JwtUtil jwtUtil;
         @Autowired
         private UserRepository userRepository;
-
+        @Value("${JWT_SECRET}")
+        private String jwtSecret;
         private String jwt;
 
         @BeforeEach
         void setUp() {
-            String secret = "THIS_IS_A_32_BYTE_LONG_SECRET_KEY!!";
+            String secret = "my_super_secret_key_that_is_long_enough_123456";
 
             User user = new User();
-            user.setEmail("4kxurdj@example.com");
-            user.setUsername("losv869dfgvhye"); // ✅ Fix
-            user.setPassword("fv96pg"); // In real life, hash it
-            user.setDisplayName("gesd");
-            user.setBio("sergt");
+            user.setEmail("vidyaaaaaaaaaaaaa.com");
+            user.setUsername("vidy"); // ✅ Fix
+            user.setPassword("vidya"); // In real life, hash it
+            user.setDisplayName("vidyaaa");
+            user.setBio("vidya's bioooooooooo, new one,");
             user.setIsActive(true);
             userRepository.save(user);
 
-            jwt = Jwts.builder()
-                    .setSubject("cvkuby@example.com")
-                    .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
-                    .compact();
+            jwt = jwtUtil.generateToken("vidyaa@example.com");
         }
 
 
         @Test
         void updateProfile_shouldReturn200AndUpdatedUser() throws Exception {
             UpdateProfileRequest request = new UpdateProfileRequest();
-            request.setDisplayName("newUser");
-            request.setBio("newBio");
+            request.setDisplayName("Vidyaaaaaaa");
+            request.setBio("vidya'sssssssssssssssssss newBio");
 
             mockMvc.perform(put("/api/users/profile")
                             .header("Authorization", "Bearer " + jwt)
